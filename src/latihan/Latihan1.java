3aabb8f9906959805e99b63fd6510089823f6a0e
Asus <Asus@DESKTOP-B57PKP8.mshome.net>
2018-09-12 11:51:17 +0700

    Initial Commit.

---
+++ src/latihan/Latihan1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan;
+
+/**
+ *
+ * @author Asus
+ */
+public class Latihan1 {
+    private static void tampilJudul(String identitas){
+        System.out.println("Identitas : "+ identitas);
+        
+        System.out.println("\nHitung Fibonacci");
+        System.out.println("1, 1, 2, 3, 5, , 13, 21, ... dst.\n");
+    }
+        public static void main(String[] args){
+            String identitas = "Sabina Okta Restati / XRPL5 / 39";
+            tampilJudul(identitas);
+        }
+    }
+

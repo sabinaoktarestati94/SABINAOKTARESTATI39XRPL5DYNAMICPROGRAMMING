650441a729890566b7fc56050296d8b6c322e98b
Asus <Asus@DESKTOP-B57PKP8.mshome.net>
2018-09-12 12:10:37 +0700

    Tambah Tampil Hasil

--- src/latihan/Latihan1.java
+++ src/latihan/Latihan1.java
@@ -26,6 +26,8 @@ public class Latihan1 {
             int n = tampilInput();
             
             BigInteger hasil = fibo(n);
+            
+            tampilHasil(n, hasil);
         }
             private static int tampilInput() {
                 Scanner scanner = new Scanner (System.in);
@@ -47,6 +49,10 @@ for (int i = 2; i < n; i++){
 }
 
 return hasil[n-1];
+}
+
+private static void tampilHasil(int n, BigInteger hasil){
+System.out.println("Bilangan Fibonacci ke-"+n+" : " + hasil);
 }
 }
             
